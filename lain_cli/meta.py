4406e19e7b8e4519b0c4df60d7d413fc0e753807
sunyi00 <sunyi00@gmail.com>
2017-12-14 13:43:41 +0800

    only output pure string for lain meta

--- lain_cli/meta.py
+++ lain_cli/meta.py
@@ -12,4 +12,4 @@ def meta():
     if meta_version is None:
         error("please git commit.")
     else:
-        info("meta version : %s" % lain_yaml(ignore_prepare=True).repo_meta_version())
+        print(lain_yaml(ignore_prepare=True).repo_meta_version())
