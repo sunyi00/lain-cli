e723296bfb419696d856836615c9d6e12f02de1e
sunyi00 <sunyi00@gmail.com>
2017-12-14 17:04:21 +0800

    bump entryclient

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ requirements = [
     'requests>=2.6.1',
     'tabulate==0.7.5',
     'einplus_lain_sdk>=2.4.1',
-    'einplus_entryclient>=2.3.2',
+    'einplus_entryclient>=2.4.0',
     'pytest<=3.2.5',
 ]
 
 dependency_links = [
-    'git+ssh://git@github.com/ein-plus/entry.git@three#egg=einplus_entryclient-2.3.2',
+    'git+ssh://git@github.com/ein-plus/entry.git@three#egg=einplus_entryclient-2.4.0',
     'git+ssh://git@github.com/ein-plus/lain-sdk.git@three#egg=einplus_lain_sdk-2.4.1'
 ]
 
