e16a16db440d3b967e4648b1430b6e6d68df8e25
timfeirg <kkcocogogo@gmail.com>
2018-08-03 12:28:18 +0800

    update requirements

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ lain = lain_cli.lain:main
 
 requirements = [
     'six>=1.9.0',
-    'PyYAML==3.12',
+    'PyYAML>=3.12',
     'argh==0.26.1',
     'argcomplete==1.9.3',
-    'humanfriendly==1.29',
+    'humanfriendly>=1.29',
     'requests>=2.6.1',
     'tabulate==0.7.5',
     'einplus_lain_sdk>=2.4.1',
-    'einplus_entryclient>=2.4.0',
+    'einplus_entryclient>=2.4.1',
 ]
 
 
